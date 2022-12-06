f77d1f2a00741fd383a9c59474b380b1633af977
Juan Moreno Beltran <jmoreno@novopayment.com>
2022-12-06 17:03:43 -0500

    POC Import as a framework for Swift 5.7.1

---
+++ RefreshViewASimple/RefreshViewASimple/Greeting.swift
@@ -0,0 +1,19 @@
+//
+//  Greeting.swift
+//  RefreshViewASimple
+//
+//  Created by Juan Moreno on 6/12/22.
+//
+
+import Foundation
+
+public class Greeting {
+    // set the initializer to public, otherwise you cannot invoke class
+    public init() {
+
+    }
+    
+    public func greet() {
+        print("Ola")
+    }
+}
---
+++ RefreshViewASimple/RefreshViewASimple/RefreshView.swift
@@ -0,0 +1,42 @@
+//
+//  RefreshView.swift
+//  ZenusApp
+//
+//  Created by Juan Moreno on 5/12/22.
+//  Copyright © 2022 Novopayment. All rights reserved.
+//
+
+import UIKit
+
+public class RefreshView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+      commonInit()
+    }
+    
+    private func commonInit() {
+      // load the nib file
+      Bundle.main.loadNibNamed("RefreshView", owner: self, options: nil)
+//      addSubview(contentView)
+//        self.bounds = CGRect(x: 20, y: 20, width: 250, height: 150)
+//      self.frame = CGRect(x: 20, y: 20, width: 250, height: 150)
+//      self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    func greet() {
+        print("Hi")
+    }
+}
